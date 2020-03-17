6c3f159b148c67d5d948ff5ad8b92ee5581f4c77
Dimitri Longo <dimitri.longo@elanathemes.fr>
2020-03-17 13:34:04 +0100

    Add Mobile Detect Library with composer.json

--- mobile-detect.php
+++ mobile-detect.php
@@ -1,21 +1,34 @@
 <?php
 namespace Grav\Plugin;
+
+use Composer\Autoload\ClassLoader;
+
 use \Grav\Common\Plugin;
 use Grav\Common\Twig\Twig;
 use RocketTheme\Toolbox\Event\Event;
 
 class MobileDetectPlugin extends Plugin
 {
-    public static function getSubscribedEvents()
-    {
-        require_once(__DIR__.'/classes/Mobile_Detect.php');
-        return [
-            'onTwigExtensions' => ['onTwigExtensions', 0],
-        ];
-    }
-    public function onTwigExtensions()
-    {
-        require_once(__DIR__ . '/twig/MobileDetectTwigExtension.php');
-        $this->grav['twig']->twig->addExtension(new MobileDetectTwigExtension());
-    }
+	public static function getSubscribedEvents()
+	{
+		return [
+			'onPluginsInitialized' => ['autoload', 100001],
+			'onTwigExtensions'     => ['onTwigExtensions', 0],
+		];
+	}
+
+	/**
+	 * [onPluginsInitialized:100000] Composer autoload.
+	 *
+	 * @return ClassLoader
+	 */
+	public function autoload()
+	{
+		return require __DIR__ . '/vendor/autoload.php';
+	}
+	public function onTwigExtensions()
+	{
+		require_once(__DIR__ . '/twig/MobileDetectTwigExtension.php');
+		$this->grav['twig']->twig->addExtension(new MobileDetectTwigExtension());
+	}
 }
